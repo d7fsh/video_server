8fbf94375ab96e3d2c35f717997d24889c2bea5b
李开明 <cfish20170329@gmail.com>
2019-08-06 20:28:35 +0800

    1. 注册路由函数的编写 2. 装饰器函数的编写(重写serveHTTP方法, 添加新的功能)

---
+++ api/api_handler.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func CreateUser(resp http.ResponseWriter, req *http.Request, ps httprouter.Params){
+
+}
---
+++ api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := RegisterHandler()
+	// 使用中间件区分要登录的请求和无需登录的请求 ---------> 校验用的session是否有效
+	// 装饰器模式, 就是原有功能的增强
+	// 1. 原有的功能需要保留
+	// 2. 原有的功能无法满足需求
+	mh := NewMiddleWareHandler(router)
+	log.Fatal(http.ListenAndServe(":8080", mh))
+}
+
+// 1. 定义一个方法, 用于返回http.Handler这个结构体实现类对象
+func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
+	m := middleWareHandler{}
+	m.router = r
+	return m
+}
+
+//2. 提供一个实现了httpHandler接口的结构体
+type middleWareHandler struct {
+	// 2.1 在中间件中保留原有的功能, 原有功能是存储在router
+	router *httprouter.Router
+}
+
+// 3. 让middleWareHandler实现ServeHttp方法
+func (m middleWareHandler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	// 3.1 增强功能, 检测session是否存在
+	validateUserSession(req)
+	// 3.2 原有功能, 原有处理请求功能
+	m.router.ServeHTTP(resp, req)
+
+}
+
+//检测session
+func validateUserSession(req *http.Request) {
+	fmt.Println("validateUserSession被调用")
+}
+
+func RegisterHandler() *httprouter.Router {
+	router := httprouter.New()
+	//router.POST("/", CreateUser)
+	return router
+}
